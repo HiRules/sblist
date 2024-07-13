5604ee1ce2189ab9c3d1ed26a8658397e34b035f
hilist <hitmp@outlook.com>
2024-07-13 19:51:24 +0800

    Update generate_rule_set.py

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -8,10 +8,6 @@ simple_site_list = [
     "https://raw.githubusercontent.com/hilist/sblist/hidden/site-red.txt"
 ]
 
-simple_ip_list = [
-    "https://raw.githubusercontent.com/hilist/sblist/hidden/ip-red.txt"
-]
-
 output_dir = "./rule-set"
 
 
@@ -39,38 +35,11 @@ def convert_site(url: str) -> str:
     return filepath
     
 
-def convert_ip(url: str) -> str:
-    r = requests.get(url)
-    ip_cidr_list = []
-    if r.status_code == 200:
-        lines = r.text.splitlines()
-        for line in lines:
-            if not line.startswith("#"):
-                ip_cidr_list.append(line)
-    result = {
-        "version": 1,
-        "rules": [
-            {
-                "ip_cidr": []
-            }
-        ]
-    }
-    filename = url.split("/")[-1]
-    result["rules"][0]["ip_cidr"] = ip_cidr_list
-    filepath = os.path.join(output_dir, filename.split(".")[-2] + ".json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
-    return filepath
-    
-
 def main():
     files = []
     os.mkdir(output_dir)
     for url in simple_site_list:
         filepath = convert_site(url)
-        files.append(filepath)
-    for url in simple_ip_list:
-        filepath = convert_ip(url)
         files.append(filepath)
     print("rule-set source generated:")
     for filepath in files:
