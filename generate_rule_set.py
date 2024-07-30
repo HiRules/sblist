3e397f7ebd5589b619e08feb428eaa4fad3687cc
HiRules <hitmp@outlook.com>
2024-07-30 22:26:34 +0800

    Update generate_rule_set.py

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -9,6 +9,10 @@ simple_site_list = [
     "https://raw.githubusercontent.com/HiRules/sblist/hidden/custom-site-proxy.txt"
 ]
 
+telegram_ip_list = [
+    "https://raw.githubusercontent.com/HiRules/sblist/hidden/telegram.txt"
+]
+
 output_dir = "./rule-set"
 
 
@@ -34,6 +38,29 @@ def convert_site(url: str) -> str:
     with open(filepath, "w") as f:
         f.write(json.dumps(result, indent=4))
     return filepath
+
+
+def convert_telegram(url: str) -> str:
+    r = requests.get(url)
+    ip_cidr_list = []
+    if r.status_code == 200:
+        lines = r.text.splitlines()
+        for line in lines:
+            if not line.startswith("#"):
+                ip_cidr_list.append(line)
+    result = {
+        "version": 1,
+        "rules": [
+            {
+                "ip_cidr": []
+            }
+        ]
+    }
+    result["rules"][0]["ip_cidr"] = ip_cidr_list
+    filepath = os.path.join(output_dir, url.split("/")[-1] + ".json")
+    with open(filepath, "w") as f:
+        f.write(json.dumps(result, indent=4))
+    return filepath
     
 
 def main():
@@ -41,6 +68,9 @@ def main():
     os.mkdir(output_dir)
     for url in simple_site_list:
         filepath = convert_site(url)
+        files.append(filepath)
+    for url in telegram_ip_list:
+        filepath = convert_telegram(url)
         files.append(filepath)
     print("rule-set source generated:")
     for filepath in files:
