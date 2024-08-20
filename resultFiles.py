fe225aec1145a1fb0d31023f111bd79360377262
HiRules <hitmp@outlook.com>
2024-08-20 12:37:20 +0800

    Update resultFiles.py

--- resultFiles.py
+++ resultFiles.py
@@ -1,8 +1,9 @@
 import requests
 import json
 import os
+import rawFiles
 from aggregate6 import aggregate
-from rawFiles import cnsite_filepath, ipv4_filepath, ipv6_filepath
+#import rawFiles import cnsite_filepath, ipv4_filepath, ipv6_filepath
 
 adguard = [
     "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
