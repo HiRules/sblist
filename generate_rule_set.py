555953a29a652d843fda77a410e12fed54d6f0cc
HiRules <hitmp@outlook.com>
2024-08-20 10:36:17 +0800

    Update generate_rule_set.py

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -29,17 +29,7 @@ maxmind = [
     "https://raw.githubusercontent.com/Dreamacro/maxmind-geoip/release/Country.mmdb"
 ]
 
-adguard = [
-    "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
-]
-
 output_dir = "./rule-set"
-fn1 = "cnsite"
-fn2 = "cnipv4"
-fn3 = "cnipv6"
-site_key = lambda x: (x.split('.')[0])
-ipv4_key = lambda x: (x.split('.')[0], x.split('.')[1], x.split('.')[2])
-ipv6_key = lambda x: (x.split(':')[0], x.split(':')[1])
 
 
 def convert_dnsmasq(url: str) -> str:
@@ -149,134 +139,6 @@ def convert_maxmind(url: str, country_code: str, ip_version: str) -> str:
     return filepath
 
 
-def convert_adguard(url: str) -> str:
-    r = requests.get(url)
-    domain_list = []
-    domain_suffix_list = []
-    domain_keyword_list = []
-    if r.status_code == 200:
-        lines = r.text.splitlines()
-        for line in lines:
-            if line.strip() == "":
-                continue
-
-            if line.startswith("!"):
-                continue
-            if line.startswith("#"):
-                continue
-
-            if line.startswith("@@"):
-                continue
-
-            if "*" in line:
-                continue
-            if "?" in line:
-                continue
-
-            if line.endswith("$important"):
-                line = line[:-10]
-
-            if line.startswith("://"):
-                if line.endswith("^"):
-                    domain_list.append(line[3:-1])
-                else:
-                    print("Warning: " + line)
-            elif line.startswith("||"):
-                if line.endswith("^"):
-                    domain_suffix_list.append(line[2:-1])
-                else:
-                    domain_keyword_list.append(line[2:])
-            elif line.startswith("|"):
-                if line.endswith("^"):
-                    domain_list.append(line[1:-1])
-                else:
-                    domain_keyword_list.append(line[1:])
-            else:
-                if line.endswith("^"):
-                    domain_suffix_list.append(line)
-    result = {
-        "version": 1,
-        "rules": [
-            {
-                "domain": [],
-                "domain_keyword": [],
-                "domain_suffix": []
-            }
-        ]
-    }
-    result["rules"][0]["domain"] = domain_list
-    result["rules"][0]["domain_keyword"] = domain_keyword_list
-    result["rules"][0]["domain_suffix"] = domain_suffix_list
-    filename = url.split("/")[-1]
-    filepath = os.path.join(output_dir, filename.split(".")[-2] + ".json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
-    return filepath
-
-
-def convert_adguard_unblock(url: str) -> str:
-    r = requests.get(url)
-    domain_suffix_list = []
-    domain_list = []
-    if r.status_code == 200:
-        lines = r.text.splitlines()
-        print("\n")
-        for line in lines:
-            if line.strip() == "":
-                continue
-
-            if line.startswith("!"):
-                continue
-            if line.startswith("#"):
-                continue
-
-            if "*" in line:
-                continue
-            if "?" in line:
-                continue
-
-            if line.endswith("$important"):
-                line = line[:-10]
-
-            if line.startswith("@@||"):
-                if line.endswith("^|"):
-                    domain_suffix_list.append(line[4:-2])
-                elif line.endswith("^"):
-                    domain_suffix_list.append(line[4:-1])
-                else:
-                    print("Warning: " + line)
-            elif line.startswith("@@|"):
-                if line.endswith("^|"):
-                    domain_list.append(line[3:-2])
-                elif line.endswith("^"):
-                    domain_list.append(line[3:-1])
-                else:
-                    print("Warning: " + line)
-            elif line.startswith("@@"):
-                if line.endswith("^|"):
-                    domain_suffix_list.append(line[2:-2])
-                elif line.endswith("^"):
-                    domain_suffix_list.append(line[2:-1])
-                else:
-                    print("Warning: " + line)
-    result = {
-        "version": 1,
-        "rules": [
-            {
-                "domain": [],
-                "domain_suffix": []
-            }
-        ]
-    }
-    result["rules"][0]["domain"] = domain_list
-    result["rules"][0]["domain_suffix"] = domain_suffix_list
-    filename = url.split("/")[-1]
-    filepath = os.path.join(output_dir, filename.split(".")[-2] + ".unblock.json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
-    return filepath
-
-
 def merge_lists(fn, kv, *lists):
     result = set()
     for i in range(len(lists)):
@@ -292,54 +154,11 @@ def merge_lists(fn, kv, *lists):
     return filepath
 
 
-def convert_site(url: str) -> str:
-    r = requests.get(url)
-    domain_suffix_list = []
-    if r.status_code == 200:
-        lines = r.text.splitlines()
-        for line in lines:
-            domain_suffix_list.append(line)
-    result = {
-        "version": 1,
-        "rules": [
-            {
-                "domain_suffix": []
-            }
-        ]
-    }
-    result["rules"][0]["domain_suffix"] = domain_suffix_list
-    filename = url.split("/")[-1]
-    filepath = os.path.join(output_dir, filename.rsplit(".",1)[0] + ".json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
-    return filepath
-
-
-def convert_ip(url: str) -> str:
-    r = requests.get(url)
-    ip_cidr_list = []
-    if r.status_code == 200:
-        lines = r.text.splitlines()
-        for line in lines:
-            ip_cidr_list.append(line)
-    result = {
-        "version": 1,
-        "rules": [
-            {
-                "ip_cidr": []
-            }
-        ]
-    }
-    result["rules"][0]["ip_cidr"] = ip_cidr_list
-    filename = url.split("/")[-1]
-    filepath = os.path.join(output_dir, filename.rsplit(".",1)[0] + ".json")
-    with open(filepath, "w") as f:
-        f.write(json.dumps(result, indent=4))
-    return filepath
-
-
 def main():
     files = []
+    site_kv = lambda x: (x.split('.')[0])
+    ipv4_kv = lambda x: (x.split('.')[0], x.split('.')[1], x.split('.')[2])
+    ipv6_kv = lambda x: (x.split(':')[0], x.split(':')[1])
     os.mkdir(output_dir)
     for url in dnsmasq_china_list:
         filepath = convert_dnsmasq(url)
@@ -360,11 +179,6 @@ def main():
         files.append(filepath)
         filepath = convert_maxmind(url, "CN", "ipv6")
         files.append(filepath)
-    for url in adguard:
-        filepath = convert_adguard(url)
-        files.append(filepath)
-        filepath = convert_adguard_unblock(url)
-        files.append(filepath)
 
     # files[0] = os.path.join(output_dir, accelerated-domains.china.txt)
     # files[1] = os.path.join(output_dir, apple.china.txt)
@@ -379,29 +193,21 @@ def main():
     # files[10] = os.path.join(output_dir, filter.conf)
     # files[11] = os.path.join(output_dir, filter.unblock.conf)
 
-    fna = os.path.join(output_dir, "test.txt")
-    fnb = os.path.join("https://raw.githubusercontent.com/HiRules/sblist/rule-set/", "test.txt")
-    print(fna)
-    print(fnb)
     print(requests.get(fnb).status_code)
     merge_site_lists = [files[0], files[1], files[2]]
-    site_filepath = merge_lists(fn1, site_key, *merge_site_lists)
+    cnsite_filepath = merge_lists("cnsite", site_kv, *merge_site_lists)
     print(site_filepath)
 
     merge_ipv4_lists = [files[3], files[4], files[6], files[8]]
-    ipv4_filepath = merge_lists(fn2, ipv4_key, *merge_ipv4_lists)
+    ipv4_filepath = merge_lists("cnipv4", ipv4_kv, *merge_ipv4_lists)
 
     merge_ipv6_lists = [files[5], files[7], files[9]]
-    ipv6_filepath = merge_lists(fn3, ipv6_key, *merge_ipv6_lists)
+    ipv6_filepath = merge_lists("cnipv6", ipv6_kv, *merge_ipv6_lists)
 
 
     print("rule-set source generated:")
     for filepath in files:
         print(filepath)
-    for filepath in files:
-        srs_path = filepath.replace(".json", ".srs")
-        os.system("sing-box rule-set compile --output " +
-                  srs_path + " " + filepath)
 
 
 
